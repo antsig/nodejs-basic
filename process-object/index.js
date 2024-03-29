c7bdc5755ed18ff0ce8b31f808f2dd99aca1662c
Sigid Priharsanto <underfighterkkh@gmail.com>
2024-03-30 02:21:43 +0800

    adding terminal run

--- process-object/index.js
+++ process-object/index.js
@@ -1,13 +1,21 @@
-const initialMemoryUsage = process.memoryUsage().heapUsed; // TODO 1
-const yourName = "Anthony Sigid"; // TODO 2
-const environment = process.env.NODE_ENV; // TODO 3
+const initialMemoryUsage = process.memoryUsage().heapUsed; 
+// TODO 1 Isi dengan nilai heapUsed dari instance process.memoryUsage.
+
+const yourName = "Anthony Sigid"; 
+// TODO 2 Isi dengan nilai index ke-2 dari process.argv.
+
+const environment = process.env.NODE_ENV; 
+// TODO 3 Isi dengan nilai NODE_ENV dari process.env.
  
 for(let i = 0; i <= 10000; i++) {
     // Proses looping ini akan membuat penggunaan memori naik
 }
  
-const currentMemoryUsage = process.memoryUsage().heapUsed; // TODO 4
+const currentMemoryUsage = process.memoryUsage().heapUsed; 
+// TODO 4 Isi dengan nilai heapUsed dari instance process.memoryUsage.
  
 console.log(`Hai, ${yourName}`);
 console.log(`Mode environment: ${environment}`)
 console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+
+// jalankan dengan SET NODE_ENV=development && node ./process-object/index.js <Nama Anda> 
