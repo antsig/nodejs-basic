9a3315d13b22bde98369d7d8a3bf50f524543e00
Sigid Priharsanto <underfighterkkh@gmail.com>
2024-03-30 02:43:31 +0800

    stream
    auto generate output.txt

---
+++ stream/index.js
@@ -0,0 +1,34 @@
+/**
+ * TODO:
+ * Buatlah program untuk membaca teks input.txt dan menuliskannya ulang pada berkas output.txt
+ * menggunakan teknik readable stream dan writable stream.
+ */
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./stream/input.txt', {
+    highWaterMark: 15
+});
+
+const writableStream = fs.createWriteStream('./stream/output.txt');
+
+readableStream.on('data', (chunk) => {
+    try {
+        writableStream.write(chunk);
+    } catch(error) {
+        // Tangkap kesalahan jika ada masalah saat menulis
+        console.error(error);
+    }
+});
+
+readableStream.on('end', () => {
+    console.log('Pemindahan data selesai.');
+    writableStream.end(); // Akhiri writable stream setelah semua data ditulis
+});
+
+writableStream.on('finish', () => {
+    console.log('Penulisan data selesai.');
+});
+
+writableStream.on('error', (error) => {
+    console.error('Terjadi kesalahan saat menulis data:', error);
+});
